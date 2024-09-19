e846b54e0e37b8d7d171f64a01cc54ad3dbf8f95
Ezekiel Ravidat <ezekiel.ravidat@gmail.com>
2024-09-19 15:35:55 +0200

    css fix

--- my-app/src/components/Article.js
+++ my-app/src/components/Article.js
@@ -1,15 +1,19 @@
 import { PrismicRichText, usePrismicDocumentByUID, PrismicImage } from '@prismicio/react'
 import { useParams } from 'react-router-dom';
+import * as prismic from '@prismicio/client'
 
 function Article() {
   const { uid } = useParams();
   const [document] = usePrismicDocumentByUID('article', uid)
+          console.log(document?.data.date)
+
 
     return (
       <div>
         <PrismicRichText field={document?.data.title} />
         <PrismicRichText field={document?.data.description} />
-        <PrismicImage field={document?.data.image}  imgixParams={{ sat: -30 }} />
+        <PrismicImage field={document?.data.image} imgixParams={{ sat: -30 }} />
+        <p>{document?.data.date}</p>
       </div>
     )
   }
