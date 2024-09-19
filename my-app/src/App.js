84259b1a19a7543251353c9bd22017d9fdcb503f
Ezekiel Ravidat <ezekiel.ravidat@gmail.com>
2024-09-19 16:33:40 +0200

    added navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import './prismic.js';
-import Home from './components/Home.js';
-import Article from './components/Article.js';
+import Home from './components/Home.jsx';
+import Article from './components/Article.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/article/:uid" element={<Article />} />
--- my-app/src/components/Article.jsx
+++ my-app/src/components/Article.jsx
@@ -1,18 +1,22 @@
 import { PrismicRichText, usePrismicDocumentByUID, PrismicImage } from '@prismicio/react'
 import { useParams } from 'react-router-dom';
 import * as prismic from '@prismicio/client'
+import * as prismicH from '@prismicio/helpers'
+
 
 function Article() {
   const { uid } = useParams();
-  const [document] = usePrismicDocumentByUID('article', uid)
-          console.log(document?.data.date)
-
+  const [document] = usePrismicDocumentByUID('article', uid);
+  const image = document?.data.image
+  const formattedImage = prismicH.asImageWidthSrcSet(image)
+  console.log(formattedImage)
 
     return (
       <div>
         <PrismicRichText field={document?.data.title} />
         <PrismicRichText field={document?.data.description} />
-        <PrismicImage field={document?.data.image} imgixParams={{ sat: -30 }} />
+        {/* <PrismicImage field={document?.data.image} imgixParams={{ sat: -30 }} /> */}
+        {/* <img src={formattedImage.src} alt="dog"/> */}
         <p>{document?.data.date}</p>
       </div>
     )
