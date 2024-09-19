489984239922cdb2280b62b3ef89f61414ca7768
Ezekiel Ravidat <ezekiel.ravidat@gmail.com>
2024-09-19 15:12:32 +0200

    fixed app router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import './prismic.js';
 import Home from './components/Home.js';
 import Article from './components/Article.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Home />
-      <Article />
-    </div>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/article/:uid" element={<Article />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
