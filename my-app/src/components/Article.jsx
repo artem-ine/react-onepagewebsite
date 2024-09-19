949600fe2795a163419c96c91a5354a6c9c2d52d
Ezekiel Ravidat <ezekiel.ravidat@gmail.com>
2024-09-19 16:50:47 +0200

    pig latinizer

--- my-app/src/components/Article.jsx
+++ my-app/src/components/Article.jsx
@@ -2,19 +2,27 @@ import { PrismicRichText, usePrismicDocumentByUID, PrismicImage } from '@prismic
 import { useParams } from 'react-router-dom';
 import * as prismic from '@prismicio/client'
 import * as prismicH from '@prismicio/helpers'
+import serializer from '../utils/Serializer';
+import PigLatin from "pig-latinizer"
+
 
 
 function Article() {
   const { uid } = useParams();
   const [document] = usePrismicDocumentByUID('article', uid);
+
   const image = document?.data.image
   const formattedImage = prismicH.asImageWidthSrcSet(image)
   console.log(formattedImage)
 
+  const pigLatin = new PigLatin();
+  const titleText = document?.data?.title ? prismicH.asText(document.data.title) : '';
+  const pigLatinTitle = titleText ? pigLatin.translate(titleText) : '';
+
     return (
       <div>
-        <PrismicRichText field={document?.data.title} />
-        <PrismicRichText field={document?.data.description} />
+        <h1>{pigLatinTitle}</h1>
+        <PrismicRichText field={document?.data.description} components={serializer} />
         {/* <PrismicImage field={document?.data.image} imgixParams={{ sat: -30 }} /> */}
         {/* <img src={formattedImage.src} alt="dog"/> */}
         <p>{document?.data.date}</p>
