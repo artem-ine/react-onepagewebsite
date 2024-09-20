021e814777342713b5930d3f769c916aa6693c48
Ezekiel Ravidat <ezekiel.ravidat@gmail.com>
2024-09-20 12:24:45 +0200

    env fix

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useAllPrismicDocumentsByType, useSinglePrismicDocument } from "@prismicio/react";
+import { useSinglePrismicDocument } from "@prismicio/react";
 import { PrismicRichText } from "@prismicio/react";
-
+import { PrismicLink } from '@prismicio/react'
 
 const Navbar = () => {
 
-  const [doc] = useSinglePrismicDocument('home')
-  const [articles] = useAllPrismicDocumentsByType('article')
-  console.log(articles)
+  const [document] = useSinglePrismicDocument('navbar')
 
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/"><PrismicRichText field={doc?.data?.homepage}/></Link>
+          <PrismicLink field={document?.data.home_link}>Home</PrismicLink>
         </li>
-        {articles?.map((art) => (
-          <li key={art.id}>
-            <Link to={`/article/${art.uid}`}><PrismicRichText field={art?.data?.title}/></Link>
-          </li>
-        ))}
+        <li>
+          <PrismicLink field={document?.data.article_links.first_article}>Lorem Ipsum</PrismicLink>
+        </li>
+        <li>
+          <PrismicLink field={document?.data.article_links.second_article}>Sed Ut</PrismicLink>
+        </li>
+        <li>
+          <PrismicLink field={document?.data.third_article}>At Vero</PrismicLink>
+        </li>
       </ul>
     </nav>
   );
